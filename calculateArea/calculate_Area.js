b648a1de423be38e70c84cf26eef9f25f79abe19
Wisdom Ogbu <wogbu55@gmail.com>
2026-01-24 10:28:21 -0500

    Grocery lis

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,10 +2,24 @@ let length;
 let width;
 
 function calculateArea() {
-    length = paresFloat(document.getElementById('length').value);
+    length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
 
 let area = length * width;
 
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
+
+let first;
+let second;
+let third;
+
+function groceryTracker() {
+    first = parseFloat(document.getElementById('first').value);
+    second = parseFloat(document.getElementById('second').value);
+    third = parseFloat(document.getElementById('third').value);
+
+let total = first + second + third;
+
+document.getElementById('amount').innerText = `The total amount is: ${total}`;
 }
