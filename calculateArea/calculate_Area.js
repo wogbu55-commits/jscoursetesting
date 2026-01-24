1ac2467969b563bf937e013ebe09118f3a5ba7b9
Wisdom Ogbu <wogbu55@gmail.com>
2026-01-24 09:57:49 -0500

    new changes

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -5,7 +5,7 @@ function calculateArea() {
     length = paresFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
 
-let areae = length * width;
+let area = length * width;
 
-document.get ElementById('result').innerText = `The area of the rectangle is: ${area}`;
+document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
